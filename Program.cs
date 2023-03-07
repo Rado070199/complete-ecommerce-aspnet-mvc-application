99b70a4f0ea118182b277569ee3601fb8f39dcdf
Rado010799 <rmazur1999@gmail.com>
2023-03-07 18:38:20 +0100

    Added CRUD functionality to the producer

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using eTickets.Data;
 using eTickets.Data.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddTransient<AppDbInitializer>();
+builder.Services.AddScoped<IProducersService, ProducersService>();
 
 var app = builder.Build();
 
