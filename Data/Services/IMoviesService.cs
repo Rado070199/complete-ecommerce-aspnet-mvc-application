60cc3482896d98a029baa57dda4f16e98f10812e
Rado010799 <rmazur1999@gmail.com>
2023-03-23 15:56:02 +0100

    Added create view + viewdata + viewbag + dropdowns

--- Data/Services/IMoviesService.cs
+++ Data/Services/IMoviesService.cs
@@ -1,10 +1,12 @@
 ﻿using eTickets.Data.Base;
 using eTickets.Models;
+using eTickets.Data.ViewModels;
 
 namespace eTickets.Data.Services
 {
     public interface IMoviesService:IEntityBaseRepository<Movie>
     {
         Task<Movie> GetMovieByIdAsync(int id);
+        Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
     }
 }
--- Data/Services/MoviesService.cs
+++ Data/Services/MoviesService.cs
@@ -1,10 +1,11 @@
 ﻿using eTickets.Data.Base;
 using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
+using eTickets.Data.ViewModels;
 
 namespace eTickets.Data.Services
 {
-    public class MoviesService:EntityBaseRepository<Movie>, IMoviesService
+    public class MoviesService : EntityBaseRepository<Movie>, IMoviesService
     {
         private readonly AppDbContext _context;
         public MoviesService(AppDbContext context) : base(context)
@@ -21,7 +22,17 @@ namespace eTickets.Data.Services
                 .FirstOrDefaultAsync(n => n.Id == id);
 
             return movieDetails;
+        }
+        public async Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues()
+        {
+            var response = new NewMovieDropdownsVM()
+            {
+                Actors = await _context.Actors.OrderBy(n => n.FullName).ToListAsync(),
+                Cinemas = await _context.Cinemas.OrderBy(n => n.Name).ToListAsync(),
+                Producers = await _context.Producers.OrderBy(n => n.FullName).ToListAsync()
+            };
 
+            return response;
         }
     }
 }
