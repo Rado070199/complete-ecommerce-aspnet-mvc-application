56a136c88ecfd7bf6507c9a4d59a8c781e31acb7
Rado010799 <rmazur1999@gmail.com>
2023-04-04 16:59:41 +0200

    added identity

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
