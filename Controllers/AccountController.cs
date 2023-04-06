23d58d3c814fcda5a82cd8c8085b6c4d8224b7df
Rado010799 <rmazur1999@gmail.com>
2023-04-06 14:44:50 +0200

    Log out users

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -73,5 +73,11 @@ namespace eTickets.Controllers
 
             return View("RegisterCompleted");
         }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
